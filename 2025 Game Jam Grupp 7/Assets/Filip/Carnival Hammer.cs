ce1042af132743e8a513d1f3fd9f0228f4437a10
Zeryth-Tempest <145355849+Zeryth-Tempest@users.noreply.github.com>
2025-05-26 13:38:10 +0200

    Charge Effect

--- 2025 Game Jam Grupp 7/Assets/Filip/Carnival Hammer.cs
+++ 2025 Game Jam Grupp 7/Assets/Filip/Carnival Hammer.cs
@@ -1,25 +1,38 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CarnivalHammer : MonoBehaviour
 {
-    private float Charge = 0f;
+    private float Charge;
 
     void Start()
     {
-        
+        float Charge = 0f;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Charge >= 0f)
+        {
+            Charge -= 0.05f;
+        }
+
+        Debug.Log("Charge");
     }
 
-    private void Press(InputValue value)
+    private void OnButtonOne(InputValue value)
     {
         if (value.isPressed)
         {
             Charge += 5f;
         }
+
+        StartCoroutine(Swing(Charge));
+    }
+
+    private IEnumerator Swing(float Charge)
+    {
+        yield return null;
     }
 }
